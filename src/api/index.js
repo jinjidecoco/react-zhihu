bcc785af73850cb14ed1197aba86d9855f0503f0
jinjidecoco <18895627215@163.com>
2018-11-27 11:02:42 +0800

    cc

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,17 @@
 import axios from 'axios'
 
+// const urlPrefix = '';
+const urlPrefix ='https://news-at.zhihu.com';
+
 export function  getNewsList(){
-	const url ='/api/4/news/latest'
+	const url = urlPrefix + '/api/4/news/latest'
 	return axios.get(url)
 }
 export function getNewsDetail(id){
-	const url ='/api/4/news/'+ id
+	const url = urlPrefix+ '/api/4/news/'+ id
 	return axios.get(url)
 }
 export function getHistoryList(date){
-	const url ='/api/4/news/before/'+ date
+	const url = urlPrefix + '/api/4/news/before/'+ date
 	return axios.get(url);
 }
